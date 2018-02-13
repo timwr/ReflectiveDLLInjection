52e655cc57e8848c993fc9d69959dc591c3d90d9
Brent Cook <busterb@gmail.com>
2018-02-13 10:11:44 -0600

    Update hash

--- dll/src/ReflectiveLoader.c
+++ dll/src/ReflectiveLoader.c
@@ -144,16 +144,18 @@ DLLEXPORT ULONG_PTR WINAPI ReflectiveLoader( VOID )
 		uiValueC = 0;
 
 		// compute the hash of the module name...
+		ULONG_PTR tmpValC = uiValueC;
 		do
 		{
-			uiValueC = ror( (DWORD)uiValueC );
+			tmpValC = ror( (DWORD)tmpValC );
 			// normalize to uppercase if the module name is in lowercase
 			if( *((BYTE *)uiValueB) >= 'a' )
-				uiValueC += *((BYTE *)uiValueB) - 0x20;
+				tmpValC += *((BYTE *)uiValueB) - 0x20;
 			else
-				uiValueC += *((BYTE *)uiValueB);
+				tmpValC += *((BYTE *)uiValueB);
 			uiValueB++;
 		} while( --usCounter );
+		uiValueC = tmpValC;
 
 		// compare the hash with that of kernel32.dll
 		if( (DWORD)uiValueC == KERNEL32DLL_HASH )
